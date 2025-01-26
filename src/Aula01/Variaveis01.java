16964154892d4fd5de3d647b60d373780f5cac9d
samueljdev <samuel.jdev11@gmail.com>
2025-01-25 23:36:33 -0400

    Initial commit

---
+++ src/Aula01/Variaveis01.java
@@ -0,0 +1,28 @@
+package Aula01;
+
+public class Variaveis01 {
+    public static void main(String[] args) {
+
+        // convenção java
+        int idade = 20;
+        String nome = "Samy";
+        String nomeDoMeuCahorro = "totó";
+
+        // aceito, mas não usado
+        int _idade;
+        int $nome;
+
+        // não é convenção java
+        String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
+        String nomeDoMeucachorro;
+
+        idade = 25;
+        System.out.println("Nome = "+ nome);
+        System.out.println("Idade = "+ idade);
+
+        // má prática, pois pode significar qualquer coisa
+        int a = 10;
+        String b = "Raio";
+    }
+}
