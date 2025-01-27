bd7ccfe0c97ce3363e2b45e14fabb8bb80c12c15
samueljdev <samuel.jdev11@gmail.com>
2025-01-27 11:03:06 -0400

    Operadores Logicos

---
+++ src/Aula01/_06OperadoresLogicos.java
@@ -0,0 +1,21 @@
+package Aula01;
+
+public class _06OperadoresLogicos {
+    public static void main(String[] args) {
+        int valor1 = 1;
+        int valor2 = 2;
+
+        boolean resultado1 = (valor1 == 1) && (valor2 == 2);
+        System.out.println("valor1 é 1 AND valor2 é 2 - resultado: " + resultado1);
+
+        boolean resultado2 = (valor1 == 1) || (valor2 == 2);
+        System.out.println("valor1 é 1 OR valor2 é 2 - resultado: " + resultado2);
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+        System.out.println(verdadeiro && falso); // ambos verdadeiro pra retorna verdadeiro
+        System.out.println(verdadeiro || falso); // ao menos um verdadeiro pra retorna verdadeiro
+        System.out.println(verdadeiro ^ falso); // apenas um verdadeiro pra retorna verdadeiro
+        System.out.println(!verdadeiro || falso);
+    }
+}
