1b9e89e4fbfd6f5739e89186108d19fe824c79d3
samueljdev <samuel.jdev11@gmail.com>
2025-01-27 00:44:25 -0400

    Operadores Relacionais

---
+++ src/Aula01/_05OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+package Aula01;
+
+public class _05OperadoresRelacionais {
+    public static void main(String[] args) {
+        // declaração + atribuição de variaveis
+        int numero1 = 1;
+        int numero2 = 2;
+
+        System.out.println("numero1 == numero2: " + (numero1 == numero2));
+        System.out.println("numero1 != numero2: " + (numero1 != numero2));
+        System.out.println("numero1 > numero2: " + (numero1 > numero2));
+        System.out.println("numero1 >= numero2: " + (numero1 >= numero2));
+        System.out.println("numero1 < numero2: " + (numero1 < numero2));
+        System.out.println("numero1 <= numero2: " + (numero1 <= numero2));
+    }
+}
